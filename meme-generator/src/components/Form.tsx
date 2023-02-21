a3e99d7de1b0f61b87076f12f943a798eb86e348
Mindaugas Stanys <mindaugasstanys173@gmail.com>
2023-02-21 15:11:49 +0200

    React practise 2023-02-21

--- meme-generator/src/components/Form.tsx
+++ meme-generator/src/components/Form.tsx
@@ -2,26 +2,41 @@ import { useState } from "react";
 import memesData from "../memesData";
 
 export const Form = () => {
-  const [firstLine, setFirstLine] = useState("");
-  const [secondLine, setSecondLine] = useState("");
+  // const [memeImage, setMemeImage] = useState("");
 
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  });
 
   const getMemeImage = () => {
-const memesArray = memesData.data.memes;
-const randomNumber = Math.floor(Math.random() * memesArray.length)
-const url = memesArray[randomNumber].url
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({...prevMeme,
+      randomImage: prevMeme.randomImage = url
+
+    }));
   };
 
-  getMemeImage();
   return (
-    <div>
-      <div className="form-input-container">
-        <input type="text" />
-        <input type="text" />
+    <>
+      <div>
+        <div className="form-input-container">
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <button onClick={getMemeImage} className="form-button" type="submit">
+          <img src="./images/get-image.png" alt="" />
+        </button>
       </div>
-      <button onClick={getMemeImage} className="form-button" type="submit">
-        <img src="./images/get-image.png" alt="" />
-      </button>
-    </div>
+
+      <div className="image-container">
+        <img className="meme-image" src={meme.randomImage} />
+      </div>
+    </>
   );
 };
